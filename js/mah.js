92bd9c334f23cd7de4f212dc19069e6f3f1d70b5
medfordanimalhospitalny <131009552+medfordanimalhospitalny@users.noreply.github.com>
2023-04-17 11:30:23 -0400

    Initial commit

---
+++ js/mah.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{	
+
+});
+
+// Collapse the navigation bar when the page is clicked anywhere else
+$(document).click(function()
+{
+	$('#navbar').collapse('hide');
+});
